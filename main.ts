156b3ccf290776b46023ee66ae1c2352aee4ad6a
Giddygangster85 <92816670+Giddygangster85@users.noreply.github.com>
2021-10-29 14:11:07 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,89 @@
+// On A being pressed a warning for a car crash ahead should show and then an animation showing two cars crashing into one another creating an explosion should show.
 input.onButtonPressed(Button.A, function () {
     basic.showString("WARNING, CAR CRASH ")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        # . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # . # #
+        # # . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        # # # # #
+        # # # # #
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # # # .
+        # . # . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # . # #
+        . . . . .
+        # # . # #
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("Stay Safe")
 })
